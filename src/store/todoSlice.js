845913a06f299a3412040a81c0ac8b4f78bc7858
BensonFeng <bensonfeng@outlook.com>
2022-04-17 12:59:00 -0400

    redux setup

---
+++ src/store/todoSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {},
+});
+
+export const {} = todoSlice.actions;
+
+export default todoSlice.reducer;
